e069f32e9266510da580f040fdc3e409b340fa0d
Derek Chen <odderbox@gmail.com>
2019-12-01 01:55:57 +0800

    add py

---
+++ c_to_f.py
@@ -0,0 +1,8 @@
+#攝氏華氏轉換公式
+#華氏= (攝氏)(9/5) + 32
+
+celsius_temperature = int(input('請輸入攝氏溫度：'))
+
+fahrenheit_temperature = (celsius_temperature * 9 / 5) + 32
+
+print('華氏溫度為：', fahrenheit_temperature)
